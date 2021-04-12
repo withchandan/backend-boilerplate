5572733c407b1e309a0b75cdb86e24ebfbb4dfe7
Chandan <sanyukt1994@gmail.com>
2021-04-12 20:23:42 +0530

    feat: add sesv2

--- src/global/service/aws/interface/ses.interface.ts
+++ src/global/service/aws/interface/ses.interface.ts
@@ -1,8 +1,10 @@
 import { DateInterface } from '../../../../shared'
 
+import { BulkEmailStatus, SubscriptionStatus } from 'aws-sdk/clients/sesv2'
+
 export interface TopicPreferenceInterface {
   topicName: string
-  subscriptionStatus: 'OPT_IN' | 'OPT_OUT' | string
+  subscriptionStatus: SubscriptionStatus
 }
 
 export interface CreateContanctReqInterface {
@@ -30,3 +32,140 @@ export interface DeleteContactReqInterface
     CreateContanctReqInterface,
     'contactListName' | 'emailAddress'
   > {}
+
+export interface TopicInterface {
+  topicName: string
+  displayName: string
+  description?: string
+  defaultSubscriptionStatus: SubscriptionStatus
+}
+
+export interface TagInterface {
+  key: string
+  value: string
+}
+
+export interface CreateContactListReqInterface {
+  contactListName: string
+  topics?: TopicInterface[]
+  description?: string
+  tags?: TagInterface[]
+}
+
+export interface ReadContactListReqInterface
+  extends Pick<CreateContactListReqInterface, 'contactListName'> {}
+
+export interface ReadContactListResInterface
+  extends CreateContactListReqInterface,
+    DateInterface {}
+
+export interface UpdateContactListReqInterface
+  extends Pick<
+    CreateContactListReqInterface,
+    'contactListName' | 'topics' | 'description'
+  > {}
+
+export interface DeleteContactListReqInterface
+  extends Pick<CreateContactListReqInterface, 'contactListName'> {}
+
+export interface TemplateContent {
+  html: string
+  text: string
+  subject: string
+}
+
+export interface CreateTemplateInterface {
+  templateName: string
+  templateContent: TemplateContent
+}
+
+export interface ReadTemplateReqInterface
+  extends Pick<CreateTemplateInterface, 'templateName'> {}
+
+export interface ReadTemplateResInterface extends CreateTemplateInterface {}
+
+export interface UpdateTemplateInterface
+  extends Pick<CreateTemplateInterface, 'templateName'> {
+  templateContent: {
+    html?: string
+    text?: string
+    subject?: string
+  }
+}
+
+export interface DeleteTemplateReqInterface
+  extends Pick<CreateTemplateInterface, 'templateName'> {}
+
+export interface TopicFilter {
+  topicName?: string
+  useDefaultIfPreferenceUnavailable?: boolean
+}
+
+export interface ListContactsFilter {
+  filteredStatus?: SubscriptionStatus
+  topicFilter?: TopicFilter
+}
+
+export interface ReadListContactsReqInterface
+  extends Pick<CreateContactListReqInterface, 'contactListName'> {
+  pageSize?: number
+  nextToken?: string
+  filter?: ListContactsFilter
+}
+
+export interface Contact
+  extends Omit<
+    CreateContanctReqInterface,
+    'contactListName' | 'attributesData'
+  > {
+  topicDefaultPreferences?: TopicPreferenceInterface[]
+}
+
+export interface ReadListContactsResInterface {
+  offset?: string
+  contacts?: Contact[]
+}
+
+export interface ReadContactListsReqInterface {
+  pageSize?: number
+  nextToken?: string
+}
+
+export interface ContactListInterface {
+  contactListName: string
+}
+
+export interface ReadContactListsResInterface {
+  contactLists: ContactListInterface[]
+  nextToken?: string
+}
+
+export interface EmailContentInterface {
+  templateName: string
+  templateData?: Record<string, string | number>
+}
+
+export interface EmailDestination {
+  to: string[]
+  cc: string[]
+  bcc: string[]
+}
+
+export interface BulkEmailEntry {
+  destination: EmailDestination
+  templateData: Record<string, string | number>
+}
+
+export interface SendBulkEmailReqInterface {
+  configurationSetName?: string
+  fromEmailAddress?: string
+  replyToAddresses?: string[]
+  defaultContent: EmailContentInterface
+  bulkEmailEntries: BulkEmailEntry[]
+}
+
+export interface SendBulkEmailResInterface {
+  status?: BulkEmailStatus
+  error?: string
+  messageId?: string
+}
--- src/global/service/aws/ses.service.ts
+++ src/global/service/aws/ses.service.ts
@@ -3,10 +3,22 @@ import { Injectable } from '@nestjs/common'
 import { SESV2 } from 'aws-sdk'
 import {
   ClientConfiguration,
+  SubscriptionStatus,
   CreateContactRequest,
   GetContactRequest,
   UpdateContactRequest,
   DeleteContactRequest,
+  CreateContactListRequest,
+  UpdateContactListRequest,
+  GetContactListRequest,
+  DeleteContactListRequest,
+  CreateEmailTemplateRequest,
+  GetEmailTemplateRequest,
+  UpdateEmailTemplateRequest,
+  DeleteEmailTemplateRequest,
+  ListContactsRequest,
+  ListContactListsRequest,
+  SendBulkEmailRequest,
 } from 'aws-sdk/clients/sesv2'
 
 import {
@@ -15,6 +27,22 @@ import {
   ReadContactResInterface,
   UpdateContactReqInterface,
   DeleteContactReqInterface,
+  CreateContactListReqInterface,
+  UpdateContactListReqInterface,
+  ReadContactListReqInterface,
+  ReadContactListResInterface,
+  DeleteContactListReqInterface,
+  CreateTemplateInterface,
+  ReadTemplateReqInterface,
+  ReadTemplateResInterface,
+  UpdateTemplateInterface,
+  DeleteTemplateReqInterface,
+  ReadListContactsReqInterface,
+  ReadListContactsResInterface,
+  ReadContactListsReqInterface,
+  ReadContactListsResInterface,
+  SendBulkEmailReqInterface,
+  SendBulkEmailResInterface,
 } from './interface'
 
 @Injectable()
@@ -96,29 +124,222 @@ export class SesService {
   }
 
   // contact list
-  public async createContactList(): Promise<void> {}
+  public async createContactList(
+    params: CreateContactListReqInterface,
+  ): Promise<void> {
+    const p: CreateContactListRequest = {
+      ContactListName: params.contactListName,
+      Topics: params.topics?.map((topic) => ({
+        TopicName: topic.topicName,
+        DisplayName: topic.displayName,
+        Description: topic.description,
+        DefaultSubscriptionStatus: topic.defaultSubscriptionStatus,
+      })),
+      Description: params.description,
+      Tags: params.tags?.map((tag) => ({ Key: tag.key, Value: tag.value })),
+    }
 
-  public async updateContactList(): Promise<void> {}
+    await this.client.createContactList(p).promise()
+  }
 
-  public async readContactList(): Promise<void> {}
+  public async updateContactList(
+    params: UpdateContactListReqInterface,
+  ): Promise<void> {
+    const p: UpdateContactListRequest = {
+      ContactListName: params.contactListName,
+      Topics: params.topics?.map((topic) => ({
+        TopicName: topic.topicName,
+        DisplayName: topic.displayName,
+        Description: topic.description,
+        DefaultSubscriptionStatus: topic.defaultSubscriptionStatus,
+      })),
+      Description: params.description,
+    }
 
-  public async deleteContactList(): Promise<void> {}
+    await this.client.updateContactList(p).promise()
+  }
+
+  public async readContactList(
+    params: ReadContactListReqInterface,
+  ): Promise<ReadContactListResInterface> {
+    const p: GetContactListRequest = { ContactListName: params.contactListName }
+
+    const contactList = await this.client.getContactList(p).promise()
+
+    return {
+      contactListName: contactList.ContactListName as string,
+      topics: contactList.Topics?.map((topic) => ({
+        topicName: topic.TopicName as string,
+        displayName: topic.DisplayName as string,
+        description: topic.Description,
+        defaultSubscriptionStatus: topic.DefaultSubscriptionStatus as SubscriptionStatus,
+      })),
+      description: contactList.Description,
+      tags: contactList.Tags?.map((tag) => ({
+        key: tag.Key,
+        value: tag.Value,
+      })),
+      createdAt: '',
+      updatedAt: '',
+    }
+  }
+
+  public async deleteContactList(
+    params: DeleteContactListReqInterface,
+  ): Promise<void> {
+    const p: DeleteContactListRequest = {
+      ContactListName: params.contactListName,
+    }
+
+    await this.client.deleteContactList(p).promise()
+  }
 
   // template
-  public async createTemplate(): Promise<void> {}
+  public async createTemplate(params: CreateTemplateInterface): Promise<void> {
+    const p: CreateEmailTemplateRequest = {
+      TemplateName: params.templateName,
+      TemplateContent: {
+        Html: params.templateContent.html,
+        Text: params.templateContent.text,
+        Subject: params.templateContent.subject,
+      },
+    }
 
-  public async readTemplate(): Promise<void> {}
+    await this.client.createEmailTemplate(p).promise()
+  }
 
-  public async updateTemplate(): Promise<void> {}
+  public async readTemplate(
+    param: ReadTemplateReqInterface,
+  ): Promise<ReadTemplateResInterface> {
+    const p: GetEmailTemplateRequest = { TemplateName: param.templateName }
 
-  public async deleteTemplate(): Promise<void> {}
+    const template = await this.client.getEmailTemplate(p).promise()
+
+    return {
+      templateName: template.TemplateName,
+      templateContent: {
+        html: template.TemplateContent.Html as string,
+        text: template.TemplateContent.Text as string,
+        subject: template.TemplateContent.Subject as string,
+      },
+    }
+  }
+
+  public async updateTemplate(params: UpdateTemplateInterface): Promise<void> {
+    const p: UpdateEmailTemplateRequest = {
+      TemplateName: params.templateName,
+      TemplateContent: {
+        Html: params.templateContent.html,
+        Text: params.templateContent.subject,
+        Subject: params.templateContent.subject,
+      },
+    }
+
+    await this.client.updateEmailTemplate(p).promise()
+  }
+
+  public async deleteTemplate(
+    params: DeleteTemplateReqInterface,
+  ): Promise<void> {
+    const p: DeleteEmailTemplateRequest = { TemplateName: params.templateName }
+
+    await this.client.deleteEmailTemplate(p).promise()
+  }
 
   // Lists the contacts present in a specific contact list.
-  public async listContacts(): Promise<void> {}
+  public async listContacts(
+    params: ReadListContactsReqInterface,
+  ): Promise<ReadListContactsResInterface> {
+    const p: ListContactsRequest = {
+      ContactListName: params.contactListName,
+      PageSize: params.pageSize,
+      NextToken: params.nextToken,
+      Filter: {
+        FilteredStatus: params.filter?.filteredStatus,
+        TopicFilter: {
+          TopicName: params.filter?.topicFilter?.topicName,
+          UseDefaultIfPreferenceUnavailable:
+            params.filter?.topicFilter?.useDefaultIfPreferenceUnavailable,
+        },
+      },
+    }
+
+    const contacts = await this.client.listContacts(p).promise()
+
+    return {
+      contacts: contacts.Contacts?.map((contact) => ({
+        emailAddress: contact.EmailAddress as string,
+        unsubscribeAll: contact.UnsubscribeAll as boolean,
+        topicPreferences: contact.TopicPreferences?.map((topicPreference) => ({
+          topicName: topicPreference.TopicName,
+          subscriptionStatus: topicPreference.SubscriptionStatus,
+        })),
+        topicDefaultPreferences: contact.TopicDefaultPreferences?.map(
+          (topicDefaultPreference) => ({
+            topicName: topicDefaultPreference.TopicName,
+            subscriptionStatus: topicDefaultPreference.SubscriptionStatus,
+          }),
+        ),
+      })),
+    }
+  }
 
   // Lists all of the contact lists available.
-  public async listContactLists(): Promise<void> {}
+  public async listContactLists(
+    params: ReadContactListsReqInterface,
+  ): Promise<ReadContactListsResInterface> {
+    const p: ListContactListsRequest = {
+      PageSize: params.pageSize,
+      NextToken: params.nextToken,
+    }
+
+    const list = await this.client.listContactLists(p).promise()
+
+    return {
+      contactLists:
+        list.ContactLists?.map((contactList) => ({
+          contactListName: contactList.ContactListName as string,
+        })) || [],
+      nextToken: list.NextToken,
+    }
+  }
 
   // Composes an email message to multiple destinations.
-  public async sendBulkEmail(): Promise<void> {}
+  public async sendBulkEmail(
+    params: SendBulkEmailReqInterface,
+  ): Promise<SendBulkEmailResInterface[]> {
+    const p: SendBulkEmailRequest = {
+      ConfigurationSetName: params.configurationSetName,
+      FromEmailAddress: params.fromEmailAddress,
+      ReplyToAddresses: params.replyToAddresses,
+      DefaultContent: {
+        Template: {
+          TemplateName: params.defaultContent.templateName,
+          TemplateData: JSON.stringify(params.defaultContent.templateData),
+        },
+      },
+      BulkEmailEntries: params.bulkEmailEntries.map((bulkEmailEntry) => ({
+        Destination: {
+          ToAddresses: bulkEmailEntry.destination.to,
+          CcAddresses: bulkEmailEntry.destination.cc,
+          BccAddresses: bulkEmailEntry.destination.bcc,
+        },
+        ReplacementEmailContent: {
+          ReplacementTemplate: {
+            ReplacementTemplateData: JSON.stringify(
+              bulkEmailEntry.templateData,
+            ),
+          },
+        },
+      })),
+    }
+
+    const res = await this.client.sendBulkEmail(p).promise()
+
+    return res.BulkEmailEntryResults.map((bulkEmailEntryResult) => ({
+      status: bulkEmailEntryResult.Status,
+      error: bulkEmailEntryResult.Error,
+      messageId: bulkEmailEntryResult.MessageId,
+    }))
+  }
 }
